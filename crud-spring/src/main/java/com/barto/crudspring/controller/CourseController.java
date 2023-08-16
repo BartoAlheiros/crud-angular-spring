f8db0065bdbc4c806f257558bf368f4ff1922e3f
BartoAlheiros <alheirosb@gmail.com>
2023-08-15 22:27:01 -0300

    classe course e coursecontroller

---
+++ crud-spring/src/main/java/com/barto/crudspring/controller/CourseController.java
@@ -0,0 +1,15 @@
+package com.barto.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+    
+    public List<Object> list() {
+        return null;
+    }
+}
