79c93df344226e284829bf6cf96191ca59190471
BartoAlheiros <alheirosb@gmail.com>
2023-08-16 21:46:37 -0300

    adicionado repositorio de cursos

--- crud-spring/src/main/java/com/barto/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/barto/crudspring/controller/CourseController.java
@@ -2,14 +2,25 @@ package com.barto.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.barto.crudspring.model.Course;
+import com.barto.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CourseController {
     
-    public List<Object> list() {
-        return null;
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
     }
 }
